09d577e502e9cc29d7a0634af11820a7058ffce7
Edward Hahn <ehahn33@gmail.com>
2016-09-29 01:11:45 -0600

    Updated to allow for overriding query param names.

--- dist/oauth-ng.js
+++ dist/oauth-ng.js
@@ -586,8 +586,8 @@ endpointClient.factory('Endpoint', ['$rootScope', 'AccessToken', '$q', '$http', 
     return params.site +
       path +
       appendChar + 'response_type=' + responseType + '&' +
-      params.clientQueryParam || 'client_id' + '=' + encodeURIComponent(params.clientId) + '&' +
-      params.redirectQueryParam || 'redirect_uri' + '=' + encodeURIComponent(params.redirectUri) + '&' +
+      (params.clientQueryParam || 'client_id') + '=' + encodeURIComponent(params.clientId) + '&' +
+      (params.redirectQueryParam || 'redirect_uri') + '=' + encodeURIComponent(params.redirectUri) + '&' +
       'scope=' + oAuthScope + '&' +
       'state=' + state + nonceParam;
   };
